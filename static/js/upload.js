c0932dcdb5299d631f4fde7cad4084f9b75f2e71
simoonoravec <simon@0r4v3c.xyz>
2024-01-07 23:57:33 +0100

    New font + Drag&Drop support

--- static/js/upload.js
+++ static/js/upload.js
@@ -8,14 +8,64 @@ $("#file").on('change', function () {
         return;
     }
 
-    $("#file-input-btn").text("Select different file");
     $("#file-name").text(fileName);
     $("#file-size").text(humanReadableSize(this.files[0].size/1000));
+    $("#input-label").hide();
+    $("#input-label-selected").show();
 });
 
+$("#droparea").on('dragover', function (e) {
+    e.preventDefault();  
+    e.stopPropagation();
+    
+    $("#input-label").text("Release your mouse to drop the file");
+});
+
+$("#droparea").on('dragleave', function (e) {
+    e.preventDefault();  
+    e.stopPropagation();
+
+    $("#input-label").text("Click to select a file or drag and drop the file here");
+});
+
+$("#droparea").on('drop', function (e) {
+    e.preventDefault();  
+    e.stopPropagation();
+
+    $("#input-label").text("Click to select a file or drag and drop the file here");
+
+    const files = e.originalEvent.dataTransfer.files;
+    if (files.length == 0) {
+        notyf.error("Only files are supported!");
+        return;
+    }
+
+    if (files.length > 1) {
+        notyf.error("Maximum 1 file at a time!");
+        return;
+    }
+
+    document.getElementById('file').files = files;
+    $('#file').trigger('change');
+});
+
+function setState(val) {
+    switch (val) {
+        case 0:
+            $("#droparea").css("pointer-events", "unset");
+            $("#upload").prop("disabled", false);
+            $("#upload").text(`Upload`);
+            break;
+
+        case 1: 
+            $("#droparea").css("pointer-events", "none");
+            $("#upload").prop("disabled", true);
+            break;
+    }
+}
+
 $("#upload").on('click', function() {
-    $("#upload-form").hide();
-    $("#upload").prop("disabled", true);
+    setState(1);
 
     $.ajax({
         type: "POST",
@@ -35,22 +85,17 @@ $("#upload").on('click', function() {
                 window.location.href = data.id;
             } else {
                 notyf.error(data.error);
-                $("#upload-form").show();
-                $("#upload").prop("disabled", false);
-                $("#upload").text(`Upload`);
+                setState(0);
             }
         },
         error: function (error) {
             notyf.error("Internal error. Try later.");
-            $("#upload-form").show();
-            $("#upload").prop("disabled", false);
-            $("#upload").text(`Upload`);
+            setState(0);
         },
         async: true,
         data: new FormData($('#upload-form')[0]),
         cache: false,
         contentType: false,
-        processData: false,
-        timeout: 60000
+        processData: false
     });
 });
